0178fe9e7688bf0d7cf682bca0cb8fe9c7710a00
MAdel7 <m.adel5880@gmail.com>
2026-03-27 15:35:02 +0200

    fixed issue

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME     || 'vendoros',
-  user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD || '',
+  connectionString: process.env.DATABASE_URL,
   max: 10,                  // max pool connections
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
